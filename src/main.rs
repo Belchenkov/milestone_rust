a751df35e71f4b30bd037b8c2143b2ae0a8a7a37
Алексей Бельченков <u608110@gmail.com>
2023-02-21 18:10:13 +0400

    MainMenu

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,57 @@
 use std::io;
 
-fn get_input() -> io::Result<String> {
+fn get_input() -> Option<String> {
     let mut buffer = String::new();
 
-    io::stdin().read_line(&mut buffer)?;
+    while io::stdin().read_line(&mut buffer).is_err() {
+        println!("Please enter your data again!");
+    }
 
-    Ok(buffer.trim().to_owned())
+    let input = buffer.trim().to_owned();
+
+    if &input == "" {
+        None
+    } else {
+        Some(input)
+    }
+
 }
 
-fn main() {
-    let mut all_input = vec![];
-    let mut times_input = 0;
-    let count_of_words = 2;
+enum MainMenu {
+    AddBill,
+    ViewBill,
+}
 
-    while times_input < count_of_words {
-        match get_input() {
-            Ok(words) => {
-                all_input.push(words);
-                times_input += 1;
-            }
-            Err(e) => println!("error: {:?}", e),
+impl MainMenu {
+    fn from_str(input: &str) -> Option<MainMenu> {
+        match input {
+            "1" => Some(Self::AddBill),
+            "2" => Some(Self::ViewBill),
+            _ => None,
         }
     }
 
-    for input in all_input {
-        println!("Original: {:?}, capitalized: {:?}", input, input.to_uppercase());
+    fn show() {
+        println!("");
+        println!(" == Bill Manager ==");
+        println!("1. Add Bill");
+        println!("2. View Bills");
+        println!("");
+        println!("Enter selection: ");
+    }
+}
+
+fn main() {
+
+    loop {
+        MainMenu::show();
+
+        let input = get_input().expect("No data entered!");
+
+        match MainMenu::from_str(input.as_str()) {
+            Some(MainMenu::AddBill) => (),
+            Some(MainMenu::ViewBill) => (),
+            None => return,
+        }
     }
 }
